0d5a9d02180b82f59cedc159f78fbac8f4fb3634
bernie <olga08_07@mail.ru>
2024-06-17 15:08:12 +0300

    Init

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update package list and install Zsh
+sudo apt update
+sudo apt install -y zsh
+
+# Install Oh My Zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Clone the Powerlevel10k theme repository
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Copy configuration files
+cp zshrc.conf ~/.zshrc
+cp p10zsh.conf ~/.p10k.zsh
+
+# Source the new Zsh configuration
+source ~/.zshrc
+
