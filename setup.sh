8ddc5c52db2a4e5d935c3e9710ffce509d1505e9
bernie <olga08_07@mail.ru>
2024-06-17 15:19:52 +0300

    Fix setup.sh and zsh conf

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/ins
 # Clone the Powerlevel10k theme repository
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
+# Clone plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
 # Copy configuration files
 cp dotfiles/zshrc.conf ~/.zshrc
 cp dotfiles/p10kzsh.conf ~/.p10k.zsh
